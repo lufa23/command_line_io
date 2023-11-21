dbc57a650595853a89bf3e72a44b9fbe76626df2
lufa23 <usuario@mind-your-business.whatever>
2023-11-21 19:42:06 +0100

    added read from file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -6,5 +7,9 @@ fn main() {
     let file_path = &args[2];
     println!("Searching for {}", query);
     println!("In file {}",file_path);
-    dbg!(args);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("The file could not be read ");
+    //dbg!(args);
+    println!("With text: \n{contents}");
 }

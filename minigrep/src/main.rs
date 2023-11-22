de08b3f8ca31746901b407b53a4b10d279c05b99
lufa23 <usuario@mind-your-business.whatever>
2023-11-22 18:46:15 +0100

    extract the args parser

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,8 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
     println!("Searching for {}", query);
     println!("In file {}",file_path);
 
@@ -12,4 +11,12 @@ fn main() {
         .expect("The file could not be read ");
     //dbg!(args);
     println!("With text: \n{contents}");
+
 }
+
+    fn parse_config(args: &[String]) -> (&str, &str) {
+        let query = &args[1];
+        let file_path = &args[2];
+
+        (query, file_path)
+    }
